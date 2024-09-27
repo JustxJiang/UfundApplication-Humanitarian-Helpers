66c7e50f8f4b3265ee35127c164fa6e3419758d6
Nick Fennelly <nfennelly01@gmail.com>
2024-09-26 20:38:02 -0400

    added search, has errors

--- ufund-api/src/main/java/com/ufund/api/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/controller/UserController.java
@@ -2,10 +2,22 @@ package com.ufund.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.ufund.api.persistence.UserDAO;
 import com.ufund.api.model.User;
 
 public class UserController {
@@ -17,6 +29,37 @@ public class UserController {
 
         // Replace below with your implementation
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Hero[]> searchHeroes(@RequestParam String name) {
+        LOG.info("GET /heroes/?name="+name);
+
+        try {
+
+            Hero[] heroArray = heroDao.getHeroes();
+            ArrayList<Hero> foundList = new ArrayList<>();
+
+            for (int i = 0; i < heroArray.length; i++) {
+                
+                if (heroArray[i].getName().indexOf(name) != -1) {
+                    foundList.add(heroArray[i]);
+                }
+
+            }
+
+            Hero[] foundArray = new Hero[foundList.size()];
+            foundList.toArray(foundArray);
+
+            return new ResponseEntity<>(foundArray, HttpStatus.OK);
+
+        }
+
+        catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
         
 }
