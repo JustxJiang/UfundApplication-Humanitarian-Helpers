0484ad58da1e6768b2749a740f42e7bec9692884
Justen Jiang <jj7932@rit.edu>
2024-09-26 20:34:02 -0400

    import

--- ufund-api/src/main/java/com/ufund/api/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.logging.Logger;
+import com.ufund.api.model.User;
 
 public class UserController {
     private static final Logger LOG = Logger.getLogger(UserController.class.getName());
