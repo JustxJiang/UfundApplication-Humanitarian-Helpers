6435b2804cfd7d191d292a9fd32db628b7b5fd91
Justen Jiang <jj7932@rit.edu>
2024-09-26 20:20:39 -0400

    started

--- ufund-api/src/main/java/com/ufund/api/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.ufund.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class UserController {
-    
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+
+    @PostMapping("")
+    public ResponseEntity<Hero> createHero(@RequestBody Hero hero) {
+        LOG.info("POST /heroes " + hero);
+
+        // Replace below with your implementation
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+        
 }
