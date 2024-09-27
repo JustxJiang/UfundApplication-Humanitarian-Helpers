dc4032a31129814e7ef0ee632996b3686e285644
Justen Jiang <jj7932@rit.edu>
2024-09-26 20:32:18 -0400

    User model done

--- ufund-api/src/main/java/com/ufund/api/model/User.java
+++ ufund-api/src/main/java/com/ufund/api/model/User.java
@@ -6,8 +6,42 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
     private static final Logger LOG = Logger.getLogger(User.class.getName());
-    
+    static final String STRING_FORMAT = "Hero [id=%d, name=%s]";
+        
     @JsonProperty("id") private int id; 
     @JsonProperty("name") private String name; 
-    
+
+    /**
+     * Create a user with the given id and name
+     * @param id The id of the user
+     * @param name The name of the user
+     * 
+     */
+    public User(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Retrieves  id of the user
+     * @return The id of the user
+     */
+    public int getId() {return id;}
+
+    /**
+     * Sets the name of the user
+     * @param name The name of the user
+     */
+    public void setName(String name) {this.name = name;}
+
+    /**
+     * Retrieves the name of the user
+     * @return The name of the user
+     */
+    public String getName() {return name;}
+
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT,id,name);
+    }
 }
