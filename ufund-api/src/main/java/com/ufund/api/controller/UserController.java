329a2b2ab1898152230ca5da4968888614347fca
Nick Fennelly <nfennelly01@gmail.com>
2024-09-30 16:17:22 -0400

    fix errors, rework implementation of some functions.

--- ufund-api/src/main/java/com/ufund/api/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,13 +22,25 @@ public class UserController {
     private static final Logger LOG = Logger.getLogger(UserController.class.getName());
     private CupboardDAO NeedDao;
 
-    /* Create Need (CRUD) */
     @PostMapping("")
-    public ResponseEntity<Need> createNeed(@RequestBody Need Need) {
-        LOG.info("POST /Needs " + Need);
+    public ResponseEntity<Need> createHero(@RequestBody Need need) {
+        LOG.info("POST /needs " + need);
 
-        // Replace below with your implementation
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        try {
+
+            if (need != null) {
+                NeedDao.createNeed(need);
+            }
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }
+
+        catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @GetMapping("/")
@@ -79,20 +92,39 @@ public class UserController {
         }
     }
 
-    public ResponseEntity<Need[]> setNeeds(@RequestParam String name, @RequestParam String newName) {
-        LOG.info("SET /Needs/?name="+name+"/?newName="+newName);
+    // public ResponseEntity<Need[]> setNeeds(@RequestParam String name, @RequestParam String newName) {
+    //     LOG.info("SET /Needs/?name="+name+"/?newName="+newName);
+    //     try {
+    //         Need[] NeedArray = NeedDao.getNeeds();
+    //         Need SettingNeed;
+    //         for (int i = 0; i < NeedArray.length; i++) {
+    //             if (NeedArray[i].getName() == name) {
+    //                 SettingNeed = NeedArray[i];
+    //                 SettingNeed.setName(newName);
+    //                 NeedDao.updateNeed(NeedArray[i], SettingNeed);
+    //             }
+    //         }
+    //         return new ResponseEntity<>(HttpStatus.OK);
+    //     } catch (Exception e) {
+    //         LOG.log(Level.SEVERE,e.getLocalizedMessage());
+    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    //     }
+    // }
+
+    @PutMapping("")
+    public ResponseEntity<Need> updateHero(@RequestBody Need need) {
+        LOG.info("PUT /needs " + need);
         try {
-            Need[] NeedArray = NeedDao.getNeeds();
-            Need SettingNeed;
-            for (int i = 0; i < NeedArray.length; i++) {
-                if (NeedArray[i].getName() == name) {
-                    SettingNeed = NeedArray[i];
-                    SettingNeed.setName(newName);
-                    NeedDao.updateNeed(NeedArray[i], SettingNeed);
-                }
+
+            if (need != null) {
+                NeedDao.updateNeed(need);
             }
+
             return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
+
+        }
+
+        catch (Exception e) {
             LOG.log(Level.SEVERE,e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
