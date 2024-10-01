6bd6bcfa8f88d595d8eae75886e82e79e873b6b3
Jr0de <dotsandbitts@gmail.com>
2024-09-30 21:13:19 -0400

    formatting

--- ufund-api/src/main/java/com/ufund/api/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/controller/UserController.java
@@ -73,9 +73,8 @@ public class UserController {
 
     }
         
-        @GetMapping("/{id}")
-        public ResponseEntity<Need> getNeed(@PathVariable int id) {
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Need> getNeed(@PathVariable int id) {
         LOG.info("GET /heroes/" + id);
         try {
             Need need = NeedDao.getNeed(id);
@@ -112,12 +111,8 @@ public class UserController {
     //     }
     // }
 
-    public ResponseEntity<Need> findNeeds(String containsText) {
-        return null;
-    }
-
     @PutMapping("")
-    public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
+    public ResponseEntity<Need> updateHero(@RequestBody Need need) {
         LOG.info("PUT /needs " + need);
         try {
 
