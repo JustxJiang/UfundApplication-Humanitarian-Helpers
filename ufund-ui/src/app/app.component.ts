f1c58db43dc41554ec2db40a4e862f79c8182fba
Justen Jiang <jj7932@rit.edu>
2024-10-21 17:40:38 -0400

    fix error temp

--- ufund-ui/src/app/app.component.ts
+++ ufund-ui/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
