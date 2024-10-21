4bdeae48a31c2ef59874295691dc47490eb2d362
Justen Jiang <jj7932@rit.edu>
2024-10-21 17:39:32 -0400

    error

--- ufund-ui/src/app/app.component.ts
+++ ufund-ui/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
