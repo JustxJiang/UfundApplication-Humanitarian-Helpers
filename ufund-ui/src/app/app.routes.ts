3a779eb1e49402f9c549e90bb68666109c974314
Justen Jiang <jj7932@rit.edu>
2024-11-05 23:19:54 -0500

    sonarstuff

--- ufund-ui/src/app/app.routes.ts
+++ ufund-ui/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { HomeComponent } from './home/home.component';
+import { CupboardComponent } from './cupboard/cupboard.component';
 
 export const routes: Routes = [
   {path: 'app', component: HomeComponent}, // HomeComponent instead of AppComponent
   {path: 'signin', component: SigninComponent},
+  {path: '/cupboard', component: CupboardComponent},
   {path: '', redirectTo: '/app', pathMatch: 'full'},
 ];
