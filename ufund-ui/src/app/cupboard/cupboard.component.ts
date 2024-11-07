63f8fa99ad447041b5fbab3394807ed0414114f5
Kelly <kelly.garcia.2377@gmail.com>
2024-11-07 14:47:50 -0500

    http client import

--- ufund-ui/src/app/cupboard/cupboard.component.ts
+++ ufund-ui/src/app/cupboard/cupboard.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-cupboard',
   standalone: true,
