3912b18a315c5f3148cdacf28cc81072f9b59e0f
Kelly <kelly.garcia.2377@gmail.com>
2024-11-06 19:38:57 -0500

    forgot password form

---
+++ ufund-ui/src/app/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-forgotpassword',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './forgotpassword.component.html',
+  styleUrl: './forgotpassword.component.css'
+})
+export class ForgotpasswordComponent {
+  restPasswordForm!: FormGroup;
+  message: string | null= null;
+  onSubmit(){
+    if(this.restPasswordForm.valid){
+      const email = this.restPasswordForm.value.email;
+      this.message = 'A link has been sent to your ${email} Please follow the instruction in the email to reset your password.';
+      this.restPasswordForm.reset();
+    }
+  }
+}
