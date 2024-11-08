0353a7403efc7981da550a61b89e51819761286c
Kelly <kelly.garcia.2377@gmail.com>
2024-11-07 19:52:10 -0500

    adoption component and pet pictures added to assets

--- ufund-ui/src/app/app.routes.ts
+++ ufund-ui/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SigninComponent } from './signin/signin.component';
 import { HomeComponent } from './home/home.component';
 import { CupboardComponent } from './cupboard/cupboard.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+// import { AdoptionComponent } from './adoption/adoption.component';
 
 export const routes: Routes = [
   {path: 'app', component: HomeComponent}, // HomeComponent instead of AppComponent
@@ -10,4 +11,6 @@ export const routes: Routes = [
   {path: 'cupboard', component: CupboardComponent},
   {path: 'forgotpassword', component: ForgotpasswordComponent},
   {path: '', redirectTo: '/app', pathMatch: 'full'},
+  // {path: 'adoptions', component: AdoptionComponent},
+  // {path: '', redirectTo:'adoptions', pathMatch: 'full'}
 ];
