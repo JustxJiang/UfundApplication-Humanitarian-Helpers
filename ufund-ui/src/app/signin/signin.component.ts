c365d9a14c66d7a161ff75086ec68b2ec9089fb6
Justen Jiang <jj7932@rit.edu>
2024-11-13 20:43:13 -0500

    signin change

--- ufund-ui/src/app/signin/signin.component.ts
+++ ufund-ui/src/app/signin/signin.component.ts
@@ -12,7 +12,7 @@ import { RouterModule, Router} from '@angular/router';
 })
 export class SigninComponent {
   // Added some fields to temp hold necessary data
-  email: string = '';
+  username: string = '';
   password : string = '';
   showPassword:boolean = false;
   isAdmin: boolean = false;
@@ -28,18 +28,18 @@ export class SigninComponent {
   submitForm(form:any){
 
     // Here we set the object types collected by the form
-    const {email, password} = form.value;
+    const {username, password} = form.value;
 
-    // Some simple logic to conver the email to lowercase, and check if
+    // Some simple logic to conver the username to lowercase, and check if
     // the user logging in is the admin account
-    if (email.toLowerCase() === "admin" && password === "admin") {
+    if (username.toLowerCase() === "admin" && password === "admin") {
       // We can then set these fields to true if so
       this.isAdmin = true;
       this.isLoggedIn = true;
       // Page alert to let you know the login was successful
       alert("Admin has logged in!")
       // Redirect the user to a page
-      this.router.navigate(['/adoption']);
+      this.router.navigate(['/needs']);
     }
 
     else {
