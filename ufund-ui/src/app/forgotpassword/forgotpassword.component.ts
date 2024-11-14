19400c3c8f926789e86b4da88f24e33c24c4d419
Justen Jiang <jj7932@rit.edu>
2024-11-13 23:59:49 -0500

    forgotpassword updates - kelly

--- ufund-ui/src/app/forgotpassword/forgotpassword.component.ts
+++ ufund-ui/src/app/forgotpassword/forgotpassword.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {  FormGroup, ReactiveFormsModule , FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -12,12 +12,22 @@ import {  FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class ForgotpasswordComponent {
   resetPasswordForm!: FormGroup;
   message: string | null= null;
-  
+
+  constructor(private fb: FormBuilder) {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
   onSubmit(){
     if(this.resetPasswordForm.valid){
       const email = this.resetPasswordForm.value.email;
-      this.message = `A link has been sent to your ${email} Please follow the instruction in the email to reset your password.`;
+      alert(`A link has been sent to ${email}. Please follow the instruction in the email to reset your password.`);
       this.resetPasswordForm.reset();
     }
+  }
+
+  closeNotification(){
+    this.message = null;
   }
 }
