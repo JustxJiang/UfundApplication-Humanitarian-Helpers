5bbc63d483a8348ff6770e86e67e16dea20032e7
maccaabdi1 <143471838+maccaabdi1@users.noreply.github.com>
2024-11-14 21:25:54 -0500

    Added test for need class

--- ufund-api/src/test/java/com/ufund/api/ufundapi/NeedTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/NeedTest.java
@@ -12,8 +12,81 @@ public class NeedTest {
     public void testCtor() {
         int expected_quantity = 99;
         String expected_name = "DogFood";
-        Need hero = new Need(expected_name, expected_quantity);
-        assertEquals(expected_quantity,hero.getQuantity());
-        assertEquals(expected_name,hero.getName());
+        Need need = new Need(expected_name, expected_quantity);
+        assertEquals(expected_quantity,need.getQuantity());
+        assertEquals(expected_name,need.getName());
+    }
+    @Test
+    public void testName() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        Need need = new Need(name,id);
+
+        String expected_name = "Galactic Agent";
+
+        // Invoke
+        need.setName(expected_name);
+
+        // Analyze
+        assertEquals(expected_name,need.getName());
+    }
+
+    @Test
+    public void testGetNeed() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int quantity = 10;
+
+       
+        Need need = new Need(name,quantity);
+        need.setId(id);
+
+       assertEquals(id, need.getId());
+       assertEquals(name, need.getName());
+       assertEquals(quantity, need.getQuantity());
+    
+    }
+    @Test
+    public void testSetNeed() {
+        // Setup
+        Need need = new Need("Toys", 5);
+
+       need.setId(42);
+       need.setName("Ball");
+       need.setQuantity(2);
+      
+       assertEquals(42, need.getId());
+       assertEquals("Ball", need.getName());
+       assertEquals(2, need.getQuantity());
+    
+    }
+    @Test
+    public void testToString() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int quantity = 10;
+
+        String expectedString = String.format(Need.STRING_FORMAT,id,name);
+       
+        Need need = new Need(name,quantity);
+        need.setId(id);
+
+       assertEquals(expectedString, need.toString());
+    
+    }
+    @Test
+    public void testDefaultValue() {
+        // Setup
+        String name = "No need";
+        Need need = new Need(name, 0);
+
+        assertEquals(0, need.getId());
+        assertEquals(name, need.getName());
+        assertEquals(0, need.getQuantity());
     }
 }
+
+
